5446a1a3210ad83ad665b5470981c4910f3374e6
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2021-01-25 03:15:01 -0800

    add geography tests

---
+++ tests/test_core.py
@@ -0,0 +1,11 @@
+import unittest
+from underleague_generator.utils import (
+    get_city_country_codes,
+    get_state_country_codes
+)
+
+
+class TestCore(unittest.TestCase):
+    def test_country_codes(self):
+        get_city_country_codes()
+        get_state_country_codes()
---
+++ tests/test_geography.py
@@ -0,0 +1,45 @@
+import random
+import unittest
+from underleague_generator.geography import (
+    StatesGeneratorBase,
+    StatesGenerator,
+    BigStatesGenerator,
+    SmallStatesGenerator,
+    CitiesGeneratorBase,
+    CitiesGenerator,
+    BigCitiesGenerator,
+    SmallTownsGenerator,
+)
+
+
+class TestGeography(unittest.TestCase):
+
+    def test_states_generator_base(self):
+        sg = StatesGeneratorBase(country_code="usa")
+        # Calling generate() should fail
+        with self.assertRaises(AttributeError):
+            sg.generate(size=50)
+
+    def test_states_generator(self):
+        random.seed(420)
+        sg = StatesGenerator(country_code="usa")
+        res = sg.generate(size=10)
+        self.assertIn("Texas", res)
+
+    def test_small_states_generator(self):
+        random.seed(420)
+        ssg = SmallStatesGenerator(country_code="usa")
+        res = ssg.generate(size=10)
+        self.assertIn("Wyoming", res)
+
+    def test_big_states_generator(self):
+        random.seed(420)
+        bsg = BigStatesGenerator(country_code="usa")
+        res = bsg.generate(size=10)
+        self.assertIn("California", res)
+    
+    ### def test_cities_generator(self):
+    ###     random.seed(420)
+    ###     cg = CitiesGenerator(country_code="usa")
+    ###     res = cg.generate(size=10)
+    ###     self.assertIn("Danbury", res)
