d9618ee90f4213ba654d7570b41ca84da2269ed8
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2021-01-28 23:14:53 -0800

    split splorts league example into default and Canadian

--- examples/splorts_league.py
+++ examples/splorts_league.py
@@ -2,7 +2,7 @@ import json
 from underleague_generator.splortsleague import SplortsLeagueGenerator
 
 
-def main():
+def default_splorts_league():
     slg = SplortsLeagueGenerator()
     res = slg.generate()
 
@@ -20,10 +20,26 @@ def main():
                 print(f" "*12 + f"{team}")
             print("\n")
 
+
+def can_splorts_league():
+    slg = SplortsLeagueGenerator(country_code="can", geo="bigcities")
+    res = slg.generate()
+
     print("\n")
     print("-"*40)
     print("\n")
 
+    for league in res:
+        print(" "*4 + f"{league} League:\n")
+        divisions = res[league]
+        for division in divisions:
+            print(" "*8 + f"{division} Division:\n")
+            teams = divisions[division]
+            for team in teams:
+                print(f" "*12 + f"{team}")
+            print("\n")
+
 
 if __name__ == "__main__":
-    main()
+    default_splorts_league()
+    can_splorts_league()
